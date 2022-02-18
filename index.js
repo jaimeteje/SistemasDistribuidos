660d1b1ab1c0d18c3d26e9b94102aaa4fb8dd06b
Jaime Tejedor Martin <jaimeteje@MacBook-Pro-de-Jaime-4.local>
2022-02-18 13:06:45 +0100

    Practica1

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => { //Para llamar a url (endpoint), devuelve el fichero html
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => { //socket
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
